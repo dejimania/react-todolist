f158fea95ff000ec417fbbb22d0139c51bc380f2
Kamil Balogun <kamilbalogun1@gmail.com>
2020-03-26 14:11:11 +0100

    Add bootstrap to project and change App component placeholder

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
-// import Button from './Counter';
+
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
