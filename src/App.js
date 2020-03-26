3dbdd7fc4d26a59a869038e03d3adfabcc91aaf1
Kamil Balogun <kamilbalogun1@gmail.com>
2020-03-26 14:18:44 +0100

    Add State Data and Data Bindings in the App component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import Button from './Counter';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: "Idrees",
+    };
+  }
   render () {
     return (
       <div className="App">
         <h4 className="bg-primary text-white text-center p-2">
-          Todo List
+          { this.state.userName } Todo List
         </h4>
         <Button></Button>
       </div>
